433592a82d33f50953707fcc89498eeb0732fcd7
Krisha Shrestha <skrisha22@tbc.edu.np>
2025-01-06 20:01:38 +0545

    migrations

---
+++ FinalProject/app/Http/Controllers/AuthManager.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Psr7\Request as Psr7Request;
+use Illuminate\Http\Request;
+
+class AuthManager extends Controller
+{
+    //
+    function login(){
+
+    }
+
+    function loginPost(Request $request){
+
+    }
+}
