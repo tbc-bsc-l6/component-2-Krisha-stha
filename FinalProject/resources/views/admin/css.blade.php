97edf487d5d47db9619b29ecd193070b7b7b1d3f
Krisha Shrestha <skrisha22@tbc.edu.np>
2025-01-07 22:56:22 +0545

    idk what errors these are, but i did fix admin and also seperated the files.

---
+++ FinalProject/resources/views/admin/css.blade.php
@@ -0,0 +1,48 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Corona Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="admin/assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="admin/assets/vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="admin/assets/vendors/jvectormap/jquery-jvectormap.css">
+    <link rel="stylesheet" href="admin/assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="admin/assets/vendors/owl-carousel-2/owl.carousel.min.css">
+    <link rel="stylesheet" href="admin/assets/vendors/owl-carousel-2/owl.theme.default.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="admin/assets/css/style.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="admin/assets/images/favicon.png" />
+    <style>
+  /* Ensure body has no overflow */
+  html, body {
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+    width: 100%;
+  }
+
+  /* Restrict any container or row to stay within the viewport width */
+  .container-scroller,
+  .row {
+    max-width: 100%;
+    overflow-x: hidden;
+  }
+
+  /* Force cards to stay within the container */
+  .card {
+    max-width: 100%;
+    overflow-x: hidden;
+  }
+
+  /* For images, canvas, or icons inside cards */
+  .card img, .card canvas, .icon-item {
+    max-width: 100%;
+    height: auto;
+  }
+</style>
--- FinalProject/resources/views/admin/home.blade.php
+++ FinalProject/resources/views/admin/home.blade.php
@@ -1,4 +1,12 @@
-<!-- Responsible for the logout button -->
-<x-app-layout>  
-
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+        @include('admin.sidebar')
+        @include('admin.navbar')
+        @include('admin.body')
+        @include('admin.script')
+  </body>
+</html>
