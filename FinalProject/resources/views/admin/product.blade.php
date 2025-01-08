e2bfdaedaf1c2863a3038b0a5c08304a4981e84c
Krisha Shrestha <skrisha22@tbc.edu.np>
2025-01-08 22:32:55 +0545

    Added and migrated a new Table called Product

--- FinalProject/resources/views/admin/product.blade.php
+++ FinalProject/resources/views/admin/product.blade.php
@@ -14,6 +14,12 @@
         color:white;
         margin-left: -175px;
       }
+
+      label
+      {
+        display: inline-block;
+        width: 200px;
+      }
     </style>
   </head>
   <body>
@@ -22,36 +28,38 @@
 
         <div class="container-fluid page-body-wrapper"  style="margin-top: 100px; margin-left: 0; padding: 0 15px; width: 300%;">
           <h1 class="title">Add Product</h1>
-       
-          <div style="padding: 100px;" class="raged">
+        <form>
+        <div style="padding-top:60px">
+          <div style="padding: 10px;" class="raged">
             <label>Product title</label>
             <input type="text" name="title" placeholder="Give a product title" >
           </div>
 
-          <div style="padding: 100px;" class="raged">
+          <div style="padding: 10px;" class="raged">
             <label>Price</label>
             <input type="number" name="price" placeholder="Give price">
           </div>
 
-          <div style="padding: 100px; " class="raged">
+          <div style="padding: 10px; " class="raged">
             <label>Description</label>
             <input type="text" name="des" placeholder="Give a description">
           </div>
 
-          <div style="padding: 100px; " class="raged">
+          <div style="padding: 10px; " class="raged">
             <label>Quantity</label>
             <input type="text" name="quantity" placeholder="Quantity">
           </div>
 
-          <div style="padding: 120px; " class="raged">
+          <div style="padding: 12px; " class="raged">
             <input type="file" name="file">
           </div>
 
           
-        <div style="padding: 20px; margin-top:10%; margin: left -500px;;" class="raged">
-            <input type="submit">
+          <div style="padding: 20px; margin-top:10%; margin: left -500px;;" class="raged">
+            <input class= "btn btn-success" type="submit">
           </div>
-
+        </div>
+        </form>
         </div>
         @include('admin.script')
   </body>
